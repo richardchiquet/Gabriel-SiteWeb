1a8d78a1464e0b7930cc850992bb667f386c6659
richardchiquet <56953330+richardchiquet@users.noreply.github.com>
2026-05-25 22:32:04 +0200

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,31 @@
-import Sidebar from"./components/Sidebar"
+import { HashRouter as Router, Routes, Route } from "react-router-dom"
+import Acceuil from "./pages/Acceuil";
+import Contact from "./pages/Contact";
+import Atelier from "./pages/L'Atelier";
+import Projets from "./pages/Projets";
+import Services from "./pages/Services";
+import Projet1 from "./pages/projets/Projet1";
+import Projet2 from "./pages/projets/Projet2";
+import Projet3 from "./pages/projets/Projet3";
+
 function App() {
 
   return (
-    <div className="flex">
-      <Sidebar></Sidebar>
-      <div className="flex-1 min-h-screen flex items-center justify-center">
-        <h1 className="text-3xl font-bold">Mon texte blq bla</h1>
-      </div>
-    </div>
+    
+    <Router>
+      <Routes>
+        <Route path="/" element={<Acceuil/>} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/atelier' element={<Atelier />} />
+        <Route path='/projets' element={<Projets />} />
+        <Route path='/services' element={<Services />} />
+        <Route path='/projets/Projet1' element={<Projet1 />} />
+        <Route path='/projets/Projet2' element={<Projet2 />} />
+        <Route path='/projets/Projet3' element={<Projet3 />} />
+
+      </Routes>
+    </Router>
+   
   )
 }
 
